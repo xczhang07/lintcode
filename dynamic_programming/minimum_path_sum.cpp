45a2a07e8b442ca8a9848d1a54060da04b6c1c11
xc-zhang <xczhang07@gmail.com>
2018-11-30 23:16:16 -0800

    Create minimum_path_sum.cpp

---
+++ dynamic_programming/minimum_path_sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /**
+     * @param grid: a list of lists of integers
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    int minPathSum(vector<vector<int>> &grid) {
+        // write your code here
+        if(grid.size() == 0 || grid[0].size() == 0)
+            return 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < n; ++i)
+            dp[0][i] = dp[0][i-1]+grid[0][i];
+        for(int i = 1; i < m; ++i)
+            dp[i][0] = dp[i-1][0]+grid[i][0];
+        for(int i = 1; i < m; ++i)
+        {
+            for(int j = 1; j < n; ++j)
+            {
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1])+grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};

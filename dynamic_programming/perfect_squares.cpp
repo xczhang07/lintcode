847dd4c3236bd5370998adbe6988ffb3d418c4c8
xc-zhang <xczhang07@gmail.com>
2018-12-01 16:57:34 -0800

    Create perfect_squares.cpp

---
+++ dynamic_programming/perfect_squares.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    /**
+     * @param n: a positive integer
+     * @return: An integer
+     */
+    int numSquares(int n) {
+        // write your code here
+        if(n == 0)
+            return 0;
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for(int i = 0; i <= n; ++i)
+        {
+            for(int j = 1; i+j*j <= n; ++j)
+                dp[i+j*j] = min(dp[i+j*j], dp[i]+1);
+        }
+        return dp[n];
+    }
+};
